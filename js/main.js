404c008769b078178da3b133aefe9c0f9189f6be
Alexander Surma <alexander.surma@gmail.com>
2011-12-20 00:25:32 +0100

    Last 10 images of tromso

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function($) {
 	var $main = $('#main');
-	var $campreview = $('<div class="camview"><h1></h1><figure><img /></figure></div>');
+	var $campreview = $('<div><h1></h1><figure><img /></figure></div>');
 	var $popup = $('<div class="alert-message" data-alert><a class="close" href="#">×</a><p></p></div>');
 	var cam_url_prefix = 'http://uk.jokkmokk.jp/photo/nr';
 	var cam_url_suffix = '/latest.jpg';
@@ -35,12 +35,15 @@
 			.success(function(data) {
 				var $imgs = $(data).find('table table[border=3] img').filter(function(idx) {
 					return img_regexp.test($(this).attr('src'));
-				}).each(function(idx, img) {
+				});
+				$($.makeArray($imgs).reverse()).each(function(idx, img) {
+					if(idx >= 10) {
+						return;
+					}
 					var src = $(img).attr('src');
-					$('<div>')
-					.append($('<h3>').text(format_date(""+img_regexp.exec(src))))
-					.append($('<img>').attr('src', tromso_img_url_prefix+src))
-					.appendTo($main);
+					var $elem = $campreview.clone().appendTo($main);
+					$elem.find('h1').text(format_date(""+img_regexp.exec(src)));
+					$elem.find('img').attr('src', tromso_img_url_prefix+src);
 				});
 			})
 			.error(function(msg) {
